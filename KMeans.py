116d0854e099e038fa339ff32dbb359256c53603
Raydar <37029157+raydar32@users.noreply.github.com>
2021-01-17 17:33:10 +0100

    Added k-mean base
    Added k-means base, now it is a really basic setup and it will be factorized in the next release.
    It is a DTW-Based k-means and it works with time series as points.

---
+++ KMeans.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 16 21:38:50 2021
+
+@author: Alessandro
+"""
+
--- anomaly_detection.py
+++ anomaly_detection.py
@@ -5,9 +5,12 @@
 import seaborn as sns
 import matplotlib.pyplot as plt                   
 import pandas as pd
+import random
+from tslearn.metrics import dtw
+import numpy as np   
 
+    
 
-         
 def mark_and_extend_anomalies(df_base,look_ahead):                  
     """
     mark_and_extend_anomalies(df_base,look_ahead):   
@@ -75,16 +78,84 @@ def plot_anomalies_heatmap(anomalies,look_ahead):
     ax.set_title(title)
     ax.xlabel = "Sensore"
     ax.ylabel = "Data"
-    plt.figure()  
+    plt.show()  
 
 
+
+    
+def k_means_assign_points(clusters,centroids):
+    for i in range(0,anomalies.shape[0]):
+        riga = anomalies.iloc[i]
+        best_fit = 0
+        distance = 10
+        #step 1: assegno ognuno al suo cluster
+        for j in range(0,len(centroids)):
+            if(dtw(riga,centroids[j])<distance):
+                best_fit = j
+                distance = (dtw(riga,centroids[j]))        
+        clusters[best_fit-1].append(i)
+    return clusters,centroids
+
+
+def k_means_recalc_centroid(anomalies,centroids):  
+    for i in range(0,len(clusters)):
+        for j in range(0,len(clusters[i])):
+            indice = clusters[i][j]
+            riga = anomalies.iloc[indice]
+            centroids[i] = centroids[i] + riga
+    
+    for k in range(0,len(centroids)):
+        centroids[k] = np.divide(centroids[k],len(clusters[k]))
+    return centroids
+
+
+def hasAnomaly(serie):
+    for item in serie:
+        if item == 1:
+            return True
+    return False
 df = import_normalize_dataset("G:\\GitHubRepo\\AnomalyDet\\Anomaly-Detection\\dataset\\originalcsv.csv")
 df = sort_dataset_by_date(df)
 df = df.T
 
-look_ahead = 7
+look_ahead = 1
 anomalies = mark_and_extend_anomalies(df,look_ahead)
-plot_anomalies_heatmap(anomalies,look_ahead)
+#plot_anomalies_heatmap(anomalies,look_ahead)
+
+clusters = [[]]
+clusters.append([])
+centroids= [[]]
+centroids.append(anomalies.iloc[random.randint(0,200)].values)
+centroids.append(anomalies.iloc[random.randint(0,200)].values)
+centroids.pop(0)
+
+
+for i in range(1,3):
+    print("Iterazione ",i)
+    clusters[0].clear()
+    clusters[1].clear()
+    clusters,centroids = k_means_assign_points(clusters,centroids)
+    centroids = k_means_recalc_centroid(anomalies,centroids)
+    
+for i in range(0,len(clusters)):
+    a = 0
+    for j in range(0,len(clusters[i])):
+        casa = anomalies.iloc[clusters[i][j]]
+        if hasAnomaly(casa):
+            a = a + 1
+    print("Cluster ", i ," ha ", a ," anomalie")
+
+
+    
+
+            
+            
+        
+    
+        
+    
+
+
 
 
         
